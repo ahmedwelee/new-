fa9c6ce04c02563915bba531243644a7dca9f94e
ahmed <ahmedpro787@gmail.com>
2025-07-19 13:37:55 +0100

    this an update

--- src/main/java/com/codewithme/store/Home/Homepage.java
+++ src/main/java/com/codewithme/store/Home/Homepage.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Homepage {
+    @RequestMapping("/")
+    public String hello() {
+        return "hello everyone this is ahmed";
+    }
 
     @RequestMapping("/bye")
     public String home() {
